102ee4af618f1387fc3f78a513e17db50fe7d4c5
Tolulope Fakunle <fakunletolulope05@gmail.com>
2023-03-03 16:37:08 +0100

    clean up

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LoginSscreen extends StatelessWidget {
+  const LoginSscreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Welcome to new projecct"),
+      ),
+    );
+  }
+}
