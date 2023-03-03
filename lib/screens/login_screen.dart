0793da3e6e52de8351d66b345e64c816d2970700
Tolulope Fakunle <fakunletolulope05@gmail.com>
2023-03-03 17:45:39 +0100

    done

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 
 class LoginSscreen extends StatelessWidget {
@@ -5,9 +6,149 @@ class LoginSscreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Welcome to new projecct"),
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              height: 400,
+              child: Stack(
+                children: <Widget>[
+                  Positioned(
+                    top: -40,
+                    height: 400,
+                    width: width,
+                    child: Container(
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                            image: AssetImage('assets/images/background.png'),
+                            fit: BoxFit.fill),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    height: 400,
+                    width: width + 20,
+                    child: Container(
+                      decoration: const BoxDecoration(
+                          image: DecorationImage(
+                              image:
+                                  AssetImage('assets/images/background-2.png'),
+                              fit: BoxFit.fill)),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  FadeInRightBig(
+                    child: const Text(
+                      "Login",
+                      style: TextStyle(
+                          color: Color.fromRGBO(49, 39, 79, 1),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.white,
+                        boxShadow: const [
+                          BoxShadow(
+                            color: Color.fromRGBO(196, 135, 198, .3),
+                            blurRadius: 20,
+                            offset: Offset(0, 10),
+                          )
+                        ]),
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          padding: const EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            border: Border(
+                              bottom: BorderSide(color: Colors.grey[200]!),
+                            ),
+                          ),
+                          child: FadeInRightBig(
+                            child: const TextField(
+                              decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: "Username",
+                                  hintStyle: TextStyle(color: Colors.grey)),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.all(10),
+                          child: FadeInLeftBig(
+                            child: const TextField(
+                              decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  hintText: "Password",
+                                  hintStyle: TextStyle(color: Colors.grey)),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  FadeInLeft(
+                    duration: const Duration(seconds: 1),
+                    child: const Center(
+                        child: Text(
+                      "Forgot Password?",
+                      style: TextStyle(color: Color.fromRGBO(196, 135, 198, 1)),
+                    )),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  FadeInRight(
+                    duration: const Duration(seconds: 1),
+                    child: Container(
+                      height: 50,
+                      margin: const EdgeInsets.symmetric(horizontal: 60),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: const Color.fromRGBO(49, 39, 79, 1),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          "Login",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  FadeInUpBig(
+                    child: const Center(
+                        child: Text(
+                      "Create Account",
+                      style: TextStyle(color: Color.fromRGBO(49, 39, 79, .6)),
+                    )),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
